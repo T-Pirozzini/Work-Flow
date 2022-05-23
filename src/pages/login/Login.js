3eadf96a64bdf301c31bc36669aca6abf73e406c
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-05-23 11:10:48 -0700

    add login form logic

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,12 +1,42 @@
+import { useState } from 'react'
+
 // styles
 import './Login.css'
 
-import React from 'react'
-
 export default function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(email, password)
+  }
+
   return (
-    <div>
-      Login
-    </div>
+    <form className="auth-form" onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <label>
+        <span>email:</span>
+        <input 
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          required 
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input 
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          required 
+        />
+      </label>
+      <button className="btn">Login</button>
+      {/* {!isPending && <button className="btn">Login</button>}
+      {isPending && <button className="btn" disabled>loading</button>}
+      {error && <div className="error">{error}</div>} */}
+    </form>
   )
 }
