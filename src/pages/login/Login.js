fb2784f2ef161f2ac86a382f3977d64f7559a475
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-05-23 11:24:03 -0700

    implement login logic

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useLogin } from '../../hooks/useLogin'
 
 // styles
 import './Login.css'
@@ -6,10 +7,11 @@ import './Login.css'
 export default function Login() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const { login, error, isPending } = useLogin()
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(email, password)
+    login(email, password)
   }
 
   return (
@@ -32,11 +34,10 @@ export default function Login() {
           value={password}
           required 
         />
-      </label>
-      <button className="btn">Login</button>
-      {/* {!isPending && <button className="btn">Login</button>}
+      </label>      
+      {!isPending && <button className="btn">Login</button>}
       {isPending && <button className="btn" disabled>loading</button>}
-      {error && <div className="error">{error}</div>} */}
+      {error && <div className="error">{error}</div>}
     </form>
   )
 }
