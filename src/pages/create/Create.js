7e1cd7a5be6df4725764b8015ddd2582c46c8777
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-05-24 10:35:42 -0700

    display avatar on sign in and display online users

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,12 +1,23 @@
+import { useState } from 'react'
+
 // styles
 import './Create.css'
 
-import React from 'react'
 
 export default function Create() {
+  // form field values
+  const [name, setName] = useState('')
+  const [details, setDetails] = useState('')
+  const [dueDate, setDueDate] = useState('')
+  const [category, setCategory] = useState('')
+  const [assignedUsers, setAssignedUsers] = useState([])
+
   return (
-    <div>
-      Create
+    <div className='create-form'>
+      <h2 className='page-title'>Create a new project</h2>
+      <form>
+
+      </form>
     </div>
   )
 }
