fd6e93485924e16f0f970d992ffc0e1f526f2782
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-05-27 09:13:45 -0700

    fetch project details

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,12 +1,25 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useDocument } from '../../hooks/useDocument'
+
 // styles
 import './Project.css'
 
-import React from 'react'
-
 export default function Project() {
+  const { id } = useParams()
+  const { error, document } = useDocument('projects', id)
+
+  // alternative way of returning a conditional error template
+  if (error) {
+    return <div className='error'>{error}</div>
+  }
+  if (!document) {
+    return <div className='loading'>Loading...</div>
+  }
+
   return (
-    <div>
-      Project
+    <div className='project-details'>
+      <h1>{document.name}</h1>      
     </div>
   )
 }
