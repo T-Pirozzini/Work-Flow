88039108f37de0458d36e87898c99eee190bd91a
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-05-27 09:53:16 -0700

    add project comments component

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import { useDocument } from '../../hooks/useDocument'
 import ProjectSummary from './ProjectSummary'
+import ProjectComments from './ProjectComments'
 
 // styles
 import './Project.css'
@@ -20,7 +21,8 @@ export default function Project() {
 
   return (
     <div className='project-details'>
-      <ProjectSummary project={document} />      
+      <ProjectSummary project={document} />
+      <ProjectComments />      
     </div>
   )
 }
