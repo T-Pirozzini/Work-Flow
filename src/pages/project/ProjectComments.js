7f073f356d3247b523f385adf173cb3a87f48d5a
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-05-29 14:50:05 -0700

    display comments real time and style content

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -35,7 +35,14 @@ export default function ProjectComments({ project }) {
         {project.comments.length > 0 && project.comments.map(comment => (
           <li key={comment.id}>
             <div className='comment-author'>
-              <Avatar src=/>
+              <Avatar src={comment.photoURL} />
+              <p>{comment.displayName}</p>
+            </div>
+            <div className="comment-date">
+              <p>date here</p>
+            </div>
+            <div className="comment-content">
+              <p>{comment.content}</p>
             </div>
           </li>
         ))}
